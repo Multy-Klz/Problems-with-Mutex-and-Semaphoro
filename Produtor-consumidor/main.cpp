d7a60fbbdbe592e160b0e24a9b15655cd53914a6
Vinícius Barbosa Mourão <34627836+MisterDoom4@users.noreply.github.com>
2022-04-29 14:19:24 -0400

    resolvendo produtor consumidor

--- Produtor-consumidor/main.cpp
+++ Produtor-consumidor/main.cpp
@@ -1,14 +1,93 @@
-// C++ program to display "Hello World"
- 
-// Header file for input output functions
+#include <Windows.h>
+#include <stdlib.h>
 #include <iostream>
-using namespace std;
- 
-// Main() function: where the execution of program begins
+#include <thread>
+#include <mutex>
+
+std::mutex mtx;
+
+#define TAM_MAX_BUFFER 10
+#define BUFFERVAZIO 0
+#define BUFFERCHEIO 1
+
+int buffer[TAM_MAX_BUFFER];
+int estado = BUFFERVAZIO;
+int fim = 0;
+int inicio = 0;
+bool sair = false;
+
+void comsumir()
+{
+    int item = 0;
+    while (sair == false)
+    {
+        if (estado == BUFFERCHEIO)
+        {
+            mtx.lock();
+            std::cout << "Comsumidor consumiu elemento: " << buffer[inicio] << std::endl;
+            buffer[inicio] = item;
+            inicio++;
+            if (inicio >= TAM_MAX_BUFFER)
+            {
+                inicio = 0;
+            }
+            mtx.unlock();
+        }
+        estado = BUFFERVAZIO;
+        Sleep(5);
+    }
+}
+
+void produzir(int id)
+{
+    int item = id;
+    while (sair == false)
+    {
+        if (estado == BUFFERVAZIO)
+        {
+            mtx.lock();
+            buffer[fim] = item;
+            std::cout << "Produtor colocou elemento: " << buffer[fim] << std::endl;
+            fim++;
+
+            if (fim >= TAM_MAX_BUFFER)
+            {
+                fim = 0;
+            }
+            item++;
+            mtx.unlock();
+        }
+        estado = BUFFERCHEIO;
+        Sleep(1);
+        
+    }
+}
+void parar()
+{
+
+    while (true)
+    {
+        if (std::cin.ignore())
+        {
+            sair = true;
+            break;
+        }
+    }
+}
 int main()
 {
-    // prints hello world
-    cout << "Hello World";
- 
+    std::thread produtor;
+    std::thread stop;
+    std::thread consumidor;
+    produtor = std::thread(produzir, 1);
+    stop = std::thread(parar);
+    consumidor = std::thread(comsumir);
+    stop.join();
+    produtor.join();
+    consumidor.join();
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << buffer[i] << std::endl;
+    }
     return 0;
 }
